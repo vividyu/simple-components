661d658d7190c192d8d7b80753d95faac7428724
Samuel <sixgod666@gmail.com>
2023-05-19 11:42:27 -0500

    add search bar

--- src/Subitem.jsx
+++ src/Subitem.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import { v4 } from 'uuid';
+const LOAD_RECORD_NUM = 3;
 
 function Subitem() {
     const [serialNum, setSerialNum] = useState(0);
     const [Item, setItem] = useState([]);
     const [keyword, setKeyword] = useState("");
+    const [loadNum, setLoadNum] = useState(LOAD_RECORD_NUM);
 
     const handleAdd = () => {
         // Compute the new serial number based on the old one
@@ -34,23 +36,56 @@ function Subitem() {
     const handleChange = (e) => {
         const curKeyword = e.target.value;
         setKeyword(curKeyword);
+        if(curKeyword === ""){
+            setLoadNum(LOAD_RECORD_NUM);
+        }
+    }
+
+    const generateLi = () => {
+        if (keyword === "") {
+            return Item.map((item) => {
+                if (item !== null) return (
+                    <li key={item.id} onClick={() => handleExpand(item.id)}><p>ID{item.title}: {item.id}</p>
+                        {item.status && <p>Subitem sn is {item.title}</p>}
+                    </li>
+                )
+            })
+        }
+
+        else {
+            return Item.filter(item => item.id.includes(keyword)).slice(0, loadNum).map((item) => {
+                if (item !== null) return (
+                    <li key={item.id} onClick={() => handleExpand(item.id)}><p>ID{item.title}: {item.id}</p>
+                        {item.status && <p>Subitem sn is {item.title}</p>}
+                    </li>
+                )
+            })
+        }
+
+    }
+
+    const handleLoadMore = () => {
+        const len = Item.length;
+        setLoadNum((prevLoadnum)=>{
+            if(prevLoadnum >= len) return len;
+            else return prevLoadnum + LOAD_RECORD_NUM;
+        })
+        console.log(loadNum);
     }
 
     return (
         <>
+            <input placeholder="Search something..." type="text" value={keyword} onChange={handleChange}></input>
             <h2>This is a subitem list</h2>
-            <input type="text" value={keyword} onChange={handleChange}></input>
+
+            {keyword === "" && <button onClick={handleAdd}>Add</button>}
+
             <ul>
-                {Item.filter(item => item.title <= keyword || keyword === "").map((item) => {
-                    if (item !== null) return (
-                        <li key={item.id} onClick={() => handleExpand(item.id)}><p>list item is {item.title}</p>
-                            {item.status && <p>Subitem sn is {item.title}</p>}
-                        </li>
-                    )
-                })}
+                {generateLi()}
             </ul>
 
-            <button onClick={handleAdd}>Add</button>
+            {keyword !== "" && <button onClick={() => handleLoadMore()}>Load more...</button>}
+
         </>
     );
 
